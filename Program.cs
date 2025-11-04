d2af23099f84c1e27203611860f8f7d2c85e9362
zenin5479 <zenin5479@gmail.com>
2025-11-04 21:48:23 +0300

    Update 0.2 Обновлен метод. Передача параметров по ссылке и значению. Выходные параметры. Модификатор out.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
+// Передача параметров по ссылке и значению
+// Выходные параметры. Модификатор out
+
 namespace ConsoleExperiment
 {
    internal class Program
    {
       static void Main()
       {
-         Console.WriteLine("Hello World!");
+         double myDr, mySqr, mySqrt, myD = 12.987;
+         int z = TrNumber(myD, out myDr, out mySqr, out mySqrt);
+         Console.WriteLine("Исходное число: {0}\nЦелая часть числа: {1}\nДробная часть числа: {2}\nКвадрат числа: {3}\nКвадратный корень числа: {4}",
+            myD, z, myDr, mySqr, mySqrt);
+
+         Console.ReadKey();
+      }
+
+      // Метод возвращающий целую и дробную части числа, квадрат и корень числа
+      static int TrNumber(double d, out double dr, out double sqr, out double sqrt)
+      {
+         int i = (int)d;
+         dr = d - i;
+         sqr = d * d;
+         sqrt = Math.Sqrt(d);
+         return i;
       }
    }
 }
