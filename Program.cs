e63e5d5afaf8154f6725f1b97e2c80d079ad05a9
zenin5479 <zenin5479@gmail.com>
2025-11-25 11:29:42 +0300

    Update 0.4 Обновлен метод. Отправка синхронных запросов с помощью HttpClient

--- Program.cs
+++ Program.cs
@@ -8,6 +8,23 @@ namespace ConsoleExperiment
    {
       static void Main()
       {
+         // Reuse these instances as needed
+         var httpClient = new HttpClient();
+         var jsonOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+
+         // Make the sync GET request
+         using (var request = new HttpRequestMessage(HttpMethod.Get, "https://api.example.com/user"))
+         {
+            var response = httpClient.Send(request);
+
+            response.EnsureSuccessStatusCode();
+
+            using var stream = response.Content.ReadAsStream();
+            var user = JsonSerializer.Deserialize<User>(stream, jsonOptions);
+
+            Console.WriteLine($"User {user.Name} is {user.Age} years old.");
+         }
+
          Console.ReadKey();
       }
    }
