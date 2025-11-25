6fdbf12aeddd12f9acbced0b4639393aa7bcbb58
zenin5479 <zenin5479@gmail.com>
2025-11-25 22:41:23 +0300

    Update 3.1 Обновлен метод. Отправка синхронных запросов с помощью HttpClient

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.IO;
 using System.Net;
 using System.Text.Json.Serialization;
@@ -15,18 +16,18 @@ namespace ConsoleExperiment
         // Наиболее простым, способом инициализации GET и POST запросов,
          // является использование объекта класса WebClient из пространства имён System.Net
          // Реализация GET запроса
-         string contents;
-         // Адрес ресурса, к которому выполняется запрос
-         string url = "https://example.com";
-         // Создаём объект WebClient
-         using (WebClient client = new WebClient())
-         {
-            client.QueryString.Add("format", "json");
-            // Выполняем запрос по адресу и получаем ответ в виде строки
-            contents = client.DownloadString(url);
-         }
+         //string contents;
+         //// Адрес ресурса, к которому выполняется запрос
+         //string url = "https://example.com";
+         //// Создаём объект WebClient
+         //using (WebClient client = new WebClient())
+         //{
+         //   client.QueryString.Add("format", "json");
+         //   // Выполняем запрос по адресу и получаем ответ в виде строки
+         //   contents = client.DownloadString(url);
+         //}
 
-         Console.WriteLine(contents);
+         //Console.WriteLine(contents);
 
          // POST запрос реализует не сложнее, но уже предполагает обязательное наличие параметров запроса 
          //string ur = "https://example.com";
@@ -34,35 +35,26 @@ namespace ConsoleExperiment
          //{
          //   webClient.QueryString.Add("format", "json");
          //   // Выполняем запрос по адресу и получаем ответ в виде строки
-         //   contents = webClient.UploadString(ur, contents);
+         //   contents = webClient.UploadString(ur, webClient);
          //   Console.WriteLine(contents);
          //}
 
-         // Создание WebClient
-         using (WebClient client = new WebClient())
+
+         WebClient client = new WebClient();
+         // Example of posting data to a server
+         string postAddress = "https://example.com";
+         // Prepare string data for POST request
+         string stringData = "name=John&age=30";
+         byte[] postData = System.Text.Encoding.ASCII.GetBytes(stringData);
+         try
          {
-            // Добавление заголовков (опционально)
-            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
-
-            // Выполнение GET-запроса и получение ответа в виде строки
-            string response = client.DownloadString("https://example.com");
-
-            // Далее можно работать с response, например, десериализовать JSON
+            byte[] response = client.UploadData(postAddress, "POST", postData);
+            // Log response headers and content
+            Console.WriteLine("Response received: " + System.Text.Encoding.ASCII.GetString(response));
          }
-
-         // Данные для отправки
-         var userData = new { Name = "John", Age = 30 };
-         string jsonData = JsonConvert.SerializeObject(userData);
-
-         using (WebClient client = new WebClient())
+         catch (Exception ex)
          {
-            // Установка заголовка Content-Type
-            client.Headers[HttpRequestHeader.ContentType] = "application/json";
-
-            // Отправка POST-запроса с JSON в теле
-            string response = client.UploadString("https://example.com", "POST", jsonData);
-
-            Console.WriteLine(response);
+            Console.WriteLine("Post failed: " + ex.Message);
          }
 
          Console.ReadKey();
