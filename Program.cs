2dc724df7b8f461e6ac6c2a63836e6021e166c04
zenin5479 <zenin5479@gmail.com>
2025-11-25 11:28:09 +0300

    Update 0.3 Обновлен метод. Отправка синхронных запросов с помощью HttpClient

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 
-// Передача параметров по ссылке и значению
-// Выходные параметры. Модификатор out
+// Отправка синхронных запросов с помощью HttpClient
 
 namespace ConsoleExperiment
 {
@@ -9,22 +8,7 @@ namespace ConsoleExperiment
    {
       static void Main()
       {
-         double myDr, mySqr, mySqrt, myD = 12.987;
-         int z = TrNumber(myD, out myDr, out mySqr, out mySqrt);
-         Console.WriteLine("Исходное число: {0}\nЦелая часть числа: {1}\nДробная часть числа: {2}\nКвадрат числа: {3}\nКвадратный корень числа: {4}",
-            myD, z, myDr, mySqr, mySqrt);
-
          Console.ReadKey();
-      }
-
-      // Метод возвращающий целую и дробную части числа, квадрат и корень числа
-      static int TrNumber(double d, out double dr, out double sqr, out double sqrt)
-      {
-         int i = (int)d;
-         dr = d - i;
-         sqr = d * d;
-         sqrt = Math.Sqrt(d);
-         return i;
       }
    }
 }
