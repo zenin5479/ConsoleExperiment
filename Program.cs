b594153856109e58ee6050f476c2f06fd11f69d4
zenin5479 <zenin5479@gmail.com>
2025-11-25 11:53:13 +0300

    Update 1.0 Обновлен метод. Отправка синхронных запросов с помощью HttpClient

--- Program.cs
+++ Program.cs
@@ -10,16 +10,16 @@ namespace ConsoleExperiment
    {
       static void Main()
       {
-         HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://example.com");
-         request.Method = "GET";
-         using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-         {
-            using (StreamReader streamReader = new StreamReader(response.GetResponseStream()))
-            {
-              string result = streamReader.ReadToEnd();
-               Console.WriteLine(result);
-            }
-         }
+         //HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://example.com");
+         //request.Method = "GET";
+         //using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+         //{
+         //   using (StreamReader streamReader = new StreamReader(response.GetResponseStream()))
+         //   {
+         //     string result = streamReader.ReadToEnd();
+         //      Console.WriteLine(result);
+         //   }
+         //}
 
          Console.ReadKey();
       }
