104746319178262bbc98216d8aa25391ee5e7392
zenin5479 <zenin5479@gmail.com>
2025-11-25 13:39:44 +0300

    Update 1.7 Обновлен метод. Отправка синхронных запросов с помощью HttpClient

--- Program.cs
+++ Program.cs
@@ -33,7 +33,18 @@ namespace ConsoleExperiment
 
          Console.WriteLine(contents);
 
-         
+
+         var httpClient = new HttpClient();
+         using (var request = new HttpRequestMessage(HttpMethod.Post, "https://localhost:12345/Coder"))
+         {
+            var json = JsonSerializer.Serialize(new Coder() { Name = "Bob", Language = "C#" });
+            request.Content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+            var response = httpClient.Send(request);
+            response.EnsureSuccessStatusCode();
+            using var streamReader = new StreamReader(response.Content.ReadAsStream());
+            Console.WriteLine(streamReader.ReadToEnd());
+         }
+
 
          Console.ReadKey();
       }
