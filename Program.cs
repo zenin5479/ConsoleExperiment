6cde0085819b0000bd6580d2abe0a7f5637b07d6
zenin5479 <zenin5479@gmail.com>
2025-11-26 09:25:35 +0300

    Update 3.9 Обновлен метод. Отправка синхронных запросов с помощью HttpClient

--- Program.cs
+++ Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Specialized;
-using System.IO;
 using System.Net;
-using System.Text.Json.Serialization;
-using Microsoft.VisualBasic;
-using Newtonsoft.Json;
 
 // Отправка синхронных запросов с помощью HttpClient
 
@@ -17,8 +12,8 @@ namespace ConsoleExperiment
          // Наиболее простым, способом инициализации GET и POST запросов,
          // является использование объекта класса WebClient из пространства имён System.Net
          // Реализация GET запроса
-        // Вариант 1
-         
+         // Вариант 1
+
          string contents;
          // Адрес ресурса, к которому выполняется запрос
          string url = "https://example.com";
@@ -27,35 +22,17 @@ namespace ConsoleExperiment
          {
             // Выполняем запрос по адресу и получаем ответ в виде строки
             contents = client.DownloadString(url);
-
          }
 
          Console.WriteLine(contents);
+         Console.WriteLine();
 
-         //Console.WriteLine(contents);
-
-         // POST запрос реализует не сложнее, но уже предполагает обязательное наличие параметров запроса 
-         //string ur = "https://example.com";
-         //using (var webClient = new WebClient())
-         //{
-         //   webClient.QueryString.Add("format", "json");
-         //   // Выполняем запрос по адресу и получаем ответ в виде строки
-         //   contents = webClient.UploadString(ur, webClient);
-         //   Console.WriteLine(contents);
-         //}
-
+         // Вариант 2
          using (WebClient client = new WebClient())
          {
             string response = client.DownloadString("https://example.com");
             Console.WriteLine(response);
          }
-
-         //using (WebClient client = new WebClient())
-         //{
-         //   string data = "key1=value1&key2=value2";
-         //   string response = client.UploadString("https://example.com/post", "POST", data);
-         //   Console.WriteLine(response);
-         //}
 
          Console.ReadKey();
       }
