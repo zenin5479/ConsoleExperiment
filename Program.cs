c4894d1cc093e2cd92d072f48dee0b5dba0489a0
zenin5479 <zenin5479@gmail.com>
2025-11-27 22:47:50 +0300

    Update 4.9 Обновлен метод. Отправка синхронных запросов с помощью WebClient

--- Program.cs
+++ Program.cs
@@ -45,7 +45,7 @@ namespace ConsoleExperiment
          }
 
          WebClient user = new WebClient();
-         string address = "https://vk.com";
+         string address = "https://example.com";
          string fileName = "Data.txt";
          try
          {
@@ -64,12 +64,12 @@ namespace ConsoleExperiment
          WebClient subject = new WebClient();
          // Метод DownloadFile() принимает два параметра - первый это путь к файлу,
          // который нужно скачать, а второй - локальное имя файла
-         subject.DownloadFile("https://www.google.ru", "WebClienUploadingFiles.txt");
+         subject.DownloadFile("https://example.com", "WebClienUploadingFiles.txt");
          Console.WriteLine("Загрузка завершена");
          // Получить вебстраницу в строку для последующей ее обработки
          // Cама процедура неэффективная - сначала загружаем страницу в файл, потом читаем текст из этого файла
          // Гораздо удобнее использовать метод OpenRead()
-         Stream str = subject.OpenRead("https://www.google.ru");
+         Stream str = subject.OpenRead("https://example.com");
          // Содержимое страницы будет загружено в переменную
          // После этого можно использовать класс StreamReader для обработки потока
          if (str != null)
