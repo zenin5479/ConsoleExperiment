b86d6d48149efa198a41b4d2bd296fcf1ee8c53c
zenin5479 <zenin5479@gmail.com>
2025-11-27 22:40:28 +0300

    Update 4.5 Обновлен метод. Отправка синхронных запросов с помощью WebClient

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Specialized;
+using System.IO;
 using System.Net;
 
 // Отправка синхронных запросов с помощью HttpClient
@@ -50,6 +51,62 @@ namespace ConsoleExperiment
             string response = System.Text.Encoding.UTF8.GetString(responseBytes);
             Console.WriteLine("POST Response: " + response);
          }
+
+         WebClient clien = new WebClient();
+         string address = "https://vk.com";
+         string fileName = "Data.txt";
+         try
+         {
+            Console.WriteLine("Загрузка данных по адресу " + address + " в файл " + fileName);
+            clien.DownloadFile(address, fileName);
+         }
+         catch (WebException exc)
+         {
+            Console.WriteLine(exc);
+         }
+
+         Console.WriteLine("Загрузка завершена");
+
+         using (WebClient user = new WebClient())
+         {
+            // Данные для отправки
+            var data = new NameValueCollection
+            {
+               { "key1", "value1" },
+               { "key2", "value2" }
+            };
+
+            // Выполняем POST-запрос
+            byte[] responseBytes = user.UploadValues("https://httpbin.org/post", "POST", data);
+            string response = System.Text.Encoding.UTF8.GetString(responseBytes);
+            Console.WriteLine("POST Response: " + response);
+         }
+
+         Console.WriteLine("Загрузка...\nПожалуйста, подождите...");
+         // Для загрузки файла используется класс WebClient
+         WebClient cli = new WebClient();
+         // Метод DownloadFile() принимает два параметра - первый это путь к файлу,
+         // который нужно скачать, а второй - локальное имя файла
+         cli.DownloadFile("https://www.google.ru", "WebClienUploadingFiles.txt");
+         Console.WriteLine("Загрузка завершена");
+         // Получить вебстраницу в строку для последующей ее обработки
+         // Cама процедура неэффективная - сначала загружаем страницу в файл, потом читаем текст из этого файла
+         // Гораздо удобнее использовать метод OpenRead()
+         Stream str = cli.OpenRead("https://www.google.ru");
+         // Содержимое страницы будет загружено в переменную
+         // После этого можно использовать класс StreamReader для обработки потока
+         if (str != null)
+         {
+            StreamReader reader = new StreamReader(str);
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+               Console.WriteLine(line);
+            }
+         }
+
+         Console.ReadLine();
+
 
          Console.ReadKey();
       }
