171abf814cd92f76adcc21f0b2a3e17811b7ca15
zenin5479 <zenin5479@gmail.com>
2025-12-07 20:32:39 +0300

    Update 6.0 Обновлен метод. Отправка синхронных запросов с помощью WebClient

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Specialized;
-using System.IO;
-using System.Net;
+using System.Collections.Generic;
 
 // Отправка синхронных запросов с помощью HttpClient
 
@@ -11,75 +9,35 @@ namespace ConsoleExperiment
    {
       static void Main()
       {
-         // Наиболее простым, способом инициализации GET и POST запросов,
-         // является использование объекта класса WebClient из пространства имён System.Net
-         // Реализация GET запроса
-
-         string contents;
-         // Адрес ресурса, к которому выполняется запрос
-         string website = "https://api.binance.com/api/v3/time";
-         // Создаём объект WebClient
-         using (WebClient client = new WebClient())
+         Dictionary<string, double> prices = new Dictionary<string, double>
          {
-            // Выполняем запрос по адресу и получаем ответ в виде строки
-            contents = client.DownloadString(website);
-            Console.WriteLine("GET Response: " + contents);
-         }
+            {"Apple", 1.2},
+            {"Banana", 0.8},
+            {"Orange", 1.5}
+         };
 
-         Console.WriteLine();
-
-         // POST - запрос
-         using (WebClient customer = new WebClient())
+         // Способ 1: Использование enumerator
+         Console.WriteLine("Using enumerator:");
+         using (var enumerator = prices.GetEnumerator())
          {
-            // Данные для отправки
-            NameValueCollection data = new NameValueCollection
+            try
             {
-               { "key1", "value1" },
-               { "key2", "value2" }
-            };
-
-            // Выполняем POST-запрос
-            byte[] responseBytes = customer.UploadValues("https://httpbin.org/post", "POST", data);
-            string response = System.Text.Encoding.UTF8.GetString(responseBytes);
-            Console.WriteLine("POST Response: " + response);
-         }
-
-         WebClient user = new WebClient();
-         string address = "https://example.com";
-         string fileName = "Data.txt";
-         try
-         {
-            Console.WriteLine("Загрузка данных по адресу " + address + " в файл " + fileName);
-            user.DownloadFile(address, fileName);
-         }
-         catch (WebException exclusion)
-         {
-            Console.WriteLine(exclusion);
-         }
-
-         Console.WriteLine("Загрузка завершена");
-
-         Console.WriteLine("Загрузка...\nПожалуйста, подождите...");
-         // Для загрузки файла используется класс WebClient
-         WebClient subject = new WebClient();
-         // Метод DownloadFile() принимает два параметра - первый это путь к файлу,
-         // который нужно скачать, а второй - локальное имя файла
-         subject.DownloadFile("https://example.com", "WebClienUploadingFiles.txt");
-         Console.WriteLine("Загрузка завершена");
-         // Получить вебстраницу в строку для последующей ее обработки
-         // Cама процедура неэффективная - сначала загружаем страницу в файл, потом читаем текст из этого файла
-         // Гораздо удобнее использовать метод OpenRead()
-         Stream brook = subject.OpenRead("https://example.com");
-         // Содержимое страницы будет загружено в переменную
-         // После этого можно использовать класс StreamReader для обработки потока
-         if (brook != null)
-         {
-            StreamReader reader = new StreamReader(brook);
-            string line;
-            while ((line = reader.ReadLine()) != null)
-            {
-               Console.WriteLine(line);
+               while (enumerator.MoveNext())
+               {
+                  Console.WriteLine($"{enumerator.Current.Key}: ${enumerator.Current.Value}");
+               }
             }
+            finally
+            {
+               enumerator.Dispose();
+            }
+         }
+
+         // Способ 2: Использование foreach (проще)
+         Console.WriteLine("\nUsing foreach:");
+         foreach (var item in prices)
+         {
+            Console.WriteLine($"{item.Key}: ${item.Value}");
          }
 
          Console.ReadKey();
