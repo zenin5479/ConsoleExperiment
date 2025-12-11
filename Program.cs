4bf628156c79dec6d6ecd28d127ebac2afaa579f
zenin5479 <zenin5479@gmail.com>
2025-12-11 22:44:21 +0300

    Update 7.3 Обновлен метод. Класс WebRequest

--- Program.cs
+++ Program.cs
@@ -36,20 +36,20 @@ namespace ConsoleExperiment
          resp.Close();
 
          // Применение класса WebClient
-         WebClient user = new WebClient();
-         string uri = "https://www.mheducation.com";
-         string fname = "data.txt";
-         try
-         {
-            Console.WriteLine("Загрузка данных по адресу " + uri + " в файл " + fname);
-            user.DownloadFile(uri, fname);
-         }
-         catch (WebException exc)
-         {
-            Console.WriteLine(exc);
-         }
+         //WebClient user = new WebClient();
+         //string uri = "https://www.mheducation.com";
+         //string fname = "data.txt";
+         //try
+         //{
+         //   Console.WriteLine("Загрузка данных по адресу " + uri + " в файл " + fname);
+         //   user.DownloadFile(uri, fname);
+         //}
+         //catch (WebException exc)
+         //{
+         //   Console.WriteLine(exc);
+         //}
 
-         Console.WriteLine("Загрузка завершена");
+         //Console.WriteLine("Загрузка завершена");
 
          Console.ReadKey();
       }
