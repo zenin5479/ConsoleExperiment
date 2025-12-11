ae09e125700e13601505ff7d07ceaeca1c2a8416
zenin5479 <zenin5479@gmail.com>
2025-12-11 22:48:52 +0300

    Update 7.5 Обновлен метод. Класс WebRequest

--- Program.cs
+++ Program.cs
@@ -10,24 +10,32 @@ namespace ConsoleExperiment
       {
          // Пример доступа к веб-сайту
          // Доступ к Интернету организуется на основе классов WebRequest и WebResponse
-         int ch;
+         int ch = 0;
          // Сначала создать объект запроса типа WebRequest по указанному URI
          HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://www.mheducation.com");
          // Затем отправить сформированный запрос и получить на него ответ
          HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
-         // Получить из ответа поток ввода.
+         // Получить из ответа поток ввода
          Stream istrm = resp.GetResponseStream();
          // А теперь прочитать и отобразить гипертекстовое содержимое, полученное по указанному URI
          // Это содержимое выводится на экран отдельными порциями по 400 символов
          // После каждой такой порции следует нажать клавишу <ENTER>, чтобы вывести на экран следующую порцию из 400 символов
          for (int i = 1; ; i++)
          {
-            ch = istrm.ReadByte();
-            if (ch == -1) break;
+            if (istrm != null)
+            {
+               ch = istrm.ReadByte();
+            }
+
+            if (ch == -1)
+            {
+               break;
+            }
+
             Console.Write((char)ch);
             if ((i % 400) == 0)
             {
-               Console.Write("\nНажмите клавишу <Enter>.");
+               Console.Write("\nНажмите клавишу <Enter>");
                Console.ReadLine();
             }
          }
