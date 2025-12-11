8130e939b8786f1153c8e9df736a76a4ce282473
zenin5479 <zenin5479@gmail.com>
2025-12-11 22:40:04 +0300

    Update 7.1 Обновлен метод. Класс WebRequest

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Net;
 
 namespace ConsoleExperiment
@@ -8,6 +8,35 @@ namespace ConsoleExperiment
    {
       static void Main()
       {
+         int ch;
+         // Сначала создать объект запроса типа WebRequest по указанному URI.
+         HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://www.mheducation.com");
+         // Затем отправить сформированный запрос и получить на него ответ.
+         HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+         // Получить из ответа поток ввода.
+         Stream istrm = resp.GetResponseStream();
+         /* А теперь прочитать и отобразить гипертекстовое содержимое,
+             полученное по указанному URI. Это содержимое выводится на экран
+             отдельными порциями по 400 символов. После каждой такой порции
+             следует нажать клавишу <ENTER>, чтобы вывести на экран
+             следующую порцию из 400 символов. */
+         for (int i = 1; ; i++)
+         {
+            ch = istrm.ReadByte();
+            if (ch == -1) break;
+            Console.Write((char)ch);
+            if ((i % 400) == 0)
+            {
+               Console.Write("\nНажмите клавишу <Enter>.");
+               Console.ReadLine();
+            }
+         }
+
+         // Закрыть ответный поток. При этом закрывается также поток ввода istrm.
+         resp.Close();
+
+
+         // Применение класса WebClient
          WebClient user = new WebClient();
          string uri = "https://www.mheducation.com";
          string fname = "data.txt";
@@ -21,7 +50,7 @@ namespace ConsoleExperiment
             Console.WriteLine(exc);
          }
 
-         Console.WriteLine("Загрузка завершена.");
+         Console.WriteLine("Загрузка завершена");
 
          Console.ReadKey();
       }
