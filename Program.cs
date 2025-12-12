938632cbbfdeaa06f0b9349db70fd838e8b5132b
zenin5479 <zenin5479@gmail.com>
2025-12-12 13:36:05 +0300

    Update 8.9 Обновлен метод. Класс WebRequest

--- Program.cs
+++ Program.cs
@@ -8,11 +8,52 @@ namespace ConsoleExperiment
    {
       static void Main()
       {
-         UsingWebClientClass();
-         ExampleAccessingWebsite();
 
+
+         ExampleAccessingWebsite();
+         ExampleAccessingSite();
+         UsingWebClientClass();
          Console.ReadKey();
       }
+
+      // Пример доступа к сайту на основе классов WebRequest и WebResponse
+      static void ExampleAccessingSite()
+      {
+         int ch = 0;
+         // Сначала создается объект запроса типа WebRequest по указанному URI
+         HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://example.com");
+         // Затем отправить сформированный запрос и получить на него ответ
+         HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+         // Получить из ответа поток ввода
+         Stream istrm = resp.GetResponseStream();
+         // А теперь прочитать и отобразить гипертекстовое содержимое, полученное по указанному URI
+         // Это содержимое выводится на экран отдельными порциями по 400 символов
+         // После каждой такой порции следует нажать клавишу <ENTER>, чтобы вывести на экран следующую порцию из 400 символов
+         for (int i = 1; ; i++)
+         {
+            if (istrm != null)
+            {
+               ch = istrm.ReadByte();
+            }
+
+            if (ch == -1)
+            {
+               break;
+            }
+
+            Console.Write((char)ch);
+            if ((i % 400) == 0)
+            {
+               Console.Write("\nНажмите клавишу <Enter>");
+               Console.ReadLine();
+            }
+         }
+
+         // Закрыть ответный поток
+         // При этом закрывается также поток ввода istrm
+         resp.Close();
+      }
+
 
       // Пример доступа к сайту на основе классов WebRequest и WebResponse
       static void ExampleAccessingWebsite()
