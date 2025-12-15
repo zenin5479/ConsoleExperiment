bab0b668599673749c03b98f35d8625496f66476
zenin5479 <zenin5479@gmail.com>
2025-12-15 13:13:41 +0300

    Update 18.1 Обновлен метод. HttpListener

--- Program.cs
+++ Program.cs
@@ -10,36 +10,39 @@ namespace ConsoleExperiment
       static void Main()
       {
          HttpListener server = new HttpListener();
-         // установка адресов прослушки
+         // Установка адресов прослушки
          server.Prefixes.Add("http://127.0.0.1:8888/connection/");
          server.Start(); // начинаем прослушивать входящие подключения
          Console.WriteLine("Сервер запущен. Ожидание подключений...");
 
-         // получаем контекст
-         var context =  server.GetContext();
-
-         var request = context.Request;  // получаем данные запроса
+         // Получаем контекст
+         HttpListenerContext context = server.GetContext();
+         // Получаем данные запроса
+         HttpListenerRequest request = context.Request;
 
          Console.WriteLine($"адрес приложения: {request.LocalEndPoint}");
          Console.WriteLine($"адрес клиента: {request.RemoteEndPoint}");
          Console.WriteLine(request.RawUrl);
          Console.WriteLine($"Запрошен адрес: {request.Url}");
          Console.WriteLine("Заголовки запроса:");
-         foreach (string item in request.Headers.Keys)
+         for (int i = 0; i < request.Headers.Keys.Count; i++)
          {
+            string item = request.Headers.Keys[i];
             Console.WriteLine($"{item}:{request.Headers[item]}");
          }
 
-         var response = context.Response;    // получаем объект для установки ответа
+         // Получаем объект для установки ответа
+         HttpListenerResponse response = context.Response;    
          byte[] buffer = Encoding.UTF8.GetBytes("Hello METANIT");
-         // получаем поток ответа и пишем в него ответ
+         // Получаем поток ответа и пишем в него ответ
          response.ContentLength64 = buffer.Length;
          using Stream output = response.OutputStream;
-         // отправляем данные
+         // Отправляем данные
          output.Write(buffer);
          output.Flush();
 
-         server.Stop(); // останавливаем сервер
+         // Останавливаем сервер
+         server.Stop(); 
 
          Console.ReadKey();
       }
