1013410a2b2528dc33e4732c356209e3a3bc19ca
zenin5479 <zenin5479@gmail.com>
2026-03-15 10:21:36 +0300

    Update 28.1 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -37,7 +37,7 @@ namespace ConsoleExperiment
          {
             iterationCount++; // Увеличиваем счётчик на каждой итерации
 
-            Console.WriteLine("Итерация №{0}", iterationCount);
+            Console.WriteLine("Итерация: {0}", iterationCount);
             Console.WriteLine();
 
             // Здесь можно разместить любую логику, выполняемую на каждой итерации
