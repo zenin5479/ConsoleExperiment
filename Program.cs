4a69021a041384146fa597371965ad536d966e7d
zenin5479 <zenin5479@gmail.com>
2026-03-15 10:37:36 +0300

    Update 29.0 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -12,8 +12,27 @@ namespace ConsoleExperiment
          //double result = CalculateConsole(inputValue);
          //Console.WriteLine(result);
 
-         double resul = Calculate(inputValue);
-         Console.WriteLine(resul);
+         //double resul = Calculate(inputValue);
+         //Console.WriteLine(resul);
+
+
+         // используем long, чтобы избежать переполнения int
+         long iterations = 0;
+         // бесконечный цикл
+         while (true)
+         {
+            iterations++;
+
+            double result = iterations * iterations;
+            Console.WriteLine("Итерация: {0}", result);
+            //Console.WriteLine();
+            //// Периодически выводим значение счётчика
+            //if (iterations % 1_000_000 == 0)
+            //{
+            //   Console.WriteLine("Выполнено итераций: {0:N0}", iterations);
+            //}
+         }
+
 
 
          //long iterations = 0; // используем long, чтобы избежать переполнения int
