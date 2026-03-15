b95c8f630cfa4aa283b560637eed93f2fd38eaf3
zenin5479 <zenin5479@gmail.com>
2026-03-15 21:44:50 +0300

    Update 30.5 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -24,12 +24,11 @@ namespace ConsoleExperiment
       private static void Calculate(double input)
       {
          // Используем long, чтобы избежать переполнения int
-         long iterations = 0; 
+         long iterations = 0;
          // Бесконечный цикл
-         while (true)         
+         while (true)
          {
             iterations++;
-
             double result = input * iterations;
             Console.WriteLine("Итерация: {0}", result);
          }
