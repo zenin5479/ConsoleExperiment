dd49c3288f91334a657e9fd4715e2bfb6e4f315d
zenin5479 <zenin5479@gmail.com>
2026-03-15 10:19:59 +0300

    Update 27.9 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -10,5 +10,21 @@ namespace ConsoleExperiment
 
          Console.ReadKey();
       }
+
+      private static int CalculateConsole(double input)
+      {
+         int iterationCount = 0; // Счётчик итераций
+
+         while (true) // Бесконечный цикл
+         {
+            iterationCount++; // Увеличиваем счётчик на каждой итерации
+
+            Console.WriteLine("Итерация №{0}", iterationCount);
+            Console.WriteLine();
+
+            // Здесь можно разместить любую логику, выполняемую на каждой итерации
+            return iterationCount;
+         }
+      }
    }
 }
