bdcc7dba94956c4fc0ff90eca23bf5dee5d8d59b
zenin5479 <zenin5479@gmail.com>
2026-03-16 09:46:39 +0300

    Update 30.5 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -12,13 +12,13 @@ namespace ConsoleExperiment
 
          Calculate(inputValue);
 
-         //long iterations = 0;
-         //while (true)
-         //{
-         //   iterations++;
-         //   double result = inputValue * iterations;
-         //   Console.WriteLine("Итерация: {0}", result);
-         //}
+         long iterations = 0;
+         while (true)
+         {
+            iterations++;
+            double result = inputValue * iterations;
+            Console.WriteLine("Итерация: {0}", result);
+         }
       }
 
       private static void Calculate(double input)
